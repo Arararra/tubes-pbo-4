666db34349a4d638ca47fbfacc60b0fcfe1d0013
Arararra <55007364+Arararra@users.noreply.github.com>
2025-03-24 07:36:45 +0700

    init project

---
+++ TubesPerpustakaan/src/main/java/com/mycompany/tubesperpustakaan/TubesPerpustakaan.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tubesperpustakaan;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class TubesPerpustakaan {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
